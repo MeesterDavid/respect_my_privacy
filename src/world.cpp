a9901ba52b86d1b1f669c53116c8684ff01576b5
MeesterDavid <david.isaacspaternostro@hu.nl>
2022-05-09 15:53:58 +0200

    notities

--- src/world.cpp
+++ src/world.cpp
@@ -2,6 +2,16 @@
  * In this world simulating game nobody cares about anything but the wind chill temperature and humidity
  */
 
+/**
+ * Dit een voorbeeld voor waarom je private/public gebruikt.
+ * 
+ */
+
+/**
+ * Een voorbeeld van een classe waar alles public is. Een van de problemen is dat elke AI (chilly bot)
+ * de wereld zomaar kan aanpassen. Zie bijv playGod in ChillyBot1
+ * 
+ */
 class World{
     public:
     float temperature;
@@ -12,6 +22,7 @@ class World{
         temperature = 0;
         humidity = 0;
         windSpeed = 0;
+        simulate();
     }
 
     void simulate(){
@@ -20,14 +31,20 @@ class World{
         
 };
 
+/**
+ * We hebben hier eenzelfde object, maar met private members en geen Setters. 
+ * Nu kan dus niet zomaar iedereen de wereld aanpassen. Maar, zoals bovenaan in dit bestand beschreven, 
+ * is niemand geinteresseerd in iets, behalve de gevoelstemperatuur. chillybot heeft een implementatie 
+ * die er vanuit gaat dat we met Fahrenheit werken. Als we nu in onze simulatie functie erachter komen
+ * dat we met celcius willen werken, dan moet iedereen die deze code gebruikt zijn/haar functie aanpassen
+ * 
+ */
 class World2{
     private:
     float temperature;
     double humidity;
     int windSpeed;
     
-
-
     void simulate(){
         /* do stuff with the world */
     }
@@ -37,6 +54,7 @@ class World2{
         temperature = 0;
         humidity = 0;
         windSpeed = 0;
+        simulate();
     }
     float getTemperature() const{
         return temperature;
@@ -50,6 +68,14 @@ class World2{
         return windSpeed;
     }
 };
+
+/**
+ * Door hier de code zo op te stellen dat je alleen de gevoelstemperatuur meegeeft hebben we minder kans
+ * dat voorgaande problemen voorkomen. Uiteraard is onze business rule "iedereen is alleen maar geinteresseerd
+ * in de gevoelstemperatuur" twijfelachtig. Maar het idee is dat je alles private maakt behalve letterlijk 
+ * de gebruiker nodig heeft.
+ * 
+ */
 
 class World3{
     private:
